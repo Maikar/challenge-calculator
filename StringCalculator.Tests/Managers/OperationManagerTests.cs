8713f4a246bb4e7c55316f1b86f295b576b7b26f
Maikar <maikar76@gmail.com>
2024-06-02 14:44:35 -0500

    feat(Req5): Values greater than 1000 are invalid

--- StringCalculator.Tests/Managers/OperationManagerTests.cs
+++ StringCalculator.Tests/Managers/OperationManagerTests.cs
@@ -12,13 +12,14 @@ public class OperationManagerTests
         _sut = new OperationManager();
     }
   
+    [InlineData("2,1001,6", 8, "2+0+6 = 8")]
     [InlineData("1\\n2,3", 6, "1+2+3 = 6")]
     [InlineData("1,2,3,4,5,6,7,8,9,10,11,12", 78, "1+2+3+4+5+6+7+8+9+10+11+12 = 78")]
     [InlineData("5,", 5, "5+0 = 5")]
     [InlineData("5,tytyt", 5, "5+0 = 5")]
     [InlineData((string?)null, 0, "0 = 0")]
     [InlineData(" ", 0, "0 = 0")]
-    [InlineData("1,5000", 5001, "1+5000 = 5001")]
+    [InlineData("1,5000", 1, "1+0 = 1")]
     [InlineData("20", 20, "20 = 20")]
     [Theory]
     public void Add_ReturnsExpectedResult(string input, int expectedResult, string expectedFormula)
--- StringCalculator/Managers/OperationManager.cs
+++ StringCalculator/Managers/OperationManager.cs
@@ -14,11 +14,30 @@ public class OperationManager : IOperationManager
         {
             throw new ArgumentOutOfRangeException(nameof(request.Input), $"Negative numbers are not allowed. Found: {string.Join(", ",negativeNumbers)}");
         }
+
+        numbers = ReplaceInvalidNumbers(numbers).ToArray();
         
         var result = numbers.Sum();
         var formula = $"{string.Join("+", numbers)} = {result}";
         
         return new AddResponse(result, formula);
+    }
+
+    private IEnumerable<int> ReplaceInvalidNumbers(int[] numbers)
+    {
+        foreach (var number in numbers)
+        {
+            if (number > 1000)
+            {
+                // Invalid numbers should be converted to 0
+                yield return 0;
+            }
+            else
+            {
+                yield return number;
+            }
+
+        }
     }
 
     private int[] GetRawNumbers(string? input)
